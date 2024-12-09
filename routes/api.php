95d984bcd9a74640dae70a8af38e3eee79112339
OusDev417 <devousse@gmail.com>
2024-12-09 19:29:09 +0100

    Last taff

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Ressources\ProductController;
 use App\Http\Controllers\Ressources\CategorieController;
+use App\Http\Controllers\API\EventController;
+use App\Http\Controllers\API\TicketController;
+use App\Http\Controllers\API\UserRewardController;
 
 
 /*
@@ -34,4 +37,38 @@ Route::group([
     'prefix'=>'hackathon'
 ], function($router){
     Route::get('',function(){return response()->json("API run well");});
+});
+
+// Routes publiques
+Route::get('events', [EventController::class, 'index']);
+Route::get('events/{event}', [EventController::class, 'show']);
+Route::get('events/featured', [EventController::class, 'featured']);
+Route::get('events/upcoming', [EventController::class, 'upcoming']);
+Route::post('events', [EventController::class, 'store']);
+// Routes authentifiées
+Route::middleware('auth:sanctum')->group(function () {
+    // Routes Événements
+    Route::put('events/{event}', [EventController::class, 'update']);
+    Route::delete('events/{event}', [EventController::class, 'destroy']);
+    
+    // Routes Tickets
+    Route::get('tickets', [TicketController::class, 'index']);
+    Route::post('tickets/purchase', [TicketController::class, 'purchase']);
+    Route::post('tickets/reserve', [TicketController::class, 'reserve']);
+    Route::post('tickets/{ticket}/transfer', [TicketController::class, 'transfer']);
+    Route::get('tickets/{ticket}/qr-code', [TicketController::class, 'generateQRCode']);
+    Route::get('tickets/{ticket}/ar-experience', [TicketController::class, 'getARExperience']);
+    Route::post('tickets/group-booking', [TicketController::class, 'groupBooking']);
+    
+    // Routes Récompenses
+    Route::get('rewards', [UserRewardController::class, 'index']);
+    Route::get('rewards/history', [UserRewardController::class, 'history']);
+    Route::get('rewards/available-perks', [UserRewardController::class, 'availablePerks']);
+    Route::post('rewards/share-social', [UserRewardController::class, 'shareSocial']);
+    Route::post('rewards/refer-friend', [UserRewardController::class, 'referFriend']);
+    
+    // Routes Organisateur
+    Route::get('organizer/events', [EventController::class, 'organizerEvents']);
+    Route::get('organizer/statistics', [EventController::class, 'statistics']);
+    Route::post('organizer/events/{event}/publish', [EventController::class, 'publish']);
 });
