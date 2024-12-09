a542772127e7c88d9ca22ae6ac37524e6d7dfcb1
OusDev417 <devousse@gmail.com>
2024-12-09 17:29:47 +0100

    first db Set

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -34,15 +34,15 @@ class ComposerAutoloaderInit579734b64504ac242a37b9931de0bdaa
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit579734b64504ac242a37b9931de0bdaa::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        };
+        }, null, null);
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
